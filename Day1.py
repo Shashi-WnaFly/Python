554dfdfa9a710c139fcfa37723e7b3c57f41a1d5
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-20 20:46:33 +0530

    Create Day1.py with some practice of python

---
+++ Day1.py
@@ -0,0 +1,23 @@
+
+# num1 = int(input("Enter number 1."))
+# num2 = int(input("Enter number 2."))
+# addition = num1 + num2;
+# print(addition)
+# print('print("The addition of two number is ", addition)', addition)
+
+print('Shashi' + ' ' + 'Anand')  # concatenation of the string
+print('My city name is japan')
+
+# print("Hello " + input("Enter Your City Namea : "))
+
+# print( len( input("Enter Your Pet Name : ") ) ) taking string and print the length.abs
+
+a = input("a: ")
+b = input("b: ")
+
+temp = a
+a = b
+b = temp
+
+print("a = " + a)
+print("b = " + b)
