b891897ae26a087d8a76e6eb32d70cd612f4ce9a
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-21 19:39:33 +0530

    Day 3 : string manupulating and some math practice

--- Datatype.py
+++ Datatype.py
@@ -11,6 +11,7 @@ num1 = 23
 num2 = 12
 print(num1 + num2)
 
+
 # float
 num3 = 1.38009
 print(num3)
@@ -21,6 +22,27 @@ print(check)
 
 num_char = len(input("What is Your Name "))
 print(type(num_char)) # this will show type 'int'
+
 # print("Your Name have "+ num_char + "characters") # throw an error because it can't concanate int and string
-int_num = str(num_char)
-print("Your Name have "+ int_num + "characters")
+
+new_num_char = str(num_char)
+print("Your Name have "+ new_num_char + " characters")
+var = False
+print("value of var is ", var)
+
+val = int(input("enter a number.\n"))
+str_val = str(val)
+sum = int(str_val[0]) + int(str_val[1])
+print(sum)
+
+# BMI
+height = float(input("Enter Your Height in m "))
+weight = float(input("Enter Your Weight in Kg "))
+
+# bmi = int(weight/(height ** 2))
+bmi = round(weight/(height ** 2), 2) # this is the another way to do this.
+# print(8//3) this will print 2 which is type int
+# bmi = (weight//(height ** 2)) but this one is type float
+print("Your BMI : ", bmi)
+
+print(f"Your Height is {height} m, Your Weight is {weight} Kg and Your BMI is {bmi}")
