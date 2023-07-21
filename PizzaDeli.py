c52a5ab0edc584b2583a95900733f51aec7711c4
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-21 21:21:32 +0530

    Day3 : updating with or operator

--- PizzaDeli.py
+++ PizzaDeli.py
@@ -5,19 +5,19 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 sum = 0
 
-if size == "S":
+if size == "S" or size == "s":
   sum += 15
-elif size == "M":
+elif size == "M" or size == "m":
   sum += 20
-elif size == "L":
+elif size == "L" or size == "l":
   sum += 25
 else:
   print("Please enter correct value")
   
-if extra_cheese == "Y":
+if extra_cheese == "Y" or extra_cheese == "y":
   sum += 1
 
-if add_pepperoni == "Y" :
+if add_pepperoni == "Y" or add_pepperoni == "y":
   if size == "S":
     sum += 2
   else:
