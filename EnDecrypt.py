435928060ca621b78bfcaf6f79f088f7852eb1cb
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-28 17:20:30 +0530

    Day8 : Update EnDecrypt.py with combine function

--- EnDecrypt.py
+++ EnDecrypt.py
@@ -38,5 +38,31 @@ while(repeat == 'yes'):
     print("error")
   repeat = input("Type 'yes' if you want to go again, otherwise type 'no'.\n")
 
+##### combine function encryption and decryption both#####
+def caeser(cipher_direction, msg, shift):
+  if(cipher_direction == "decode"):
+    shift *= -1
+  new_msg = ""
+  for letter in msg:
+    char = ord(letter) + shift
+    if(char > 122):
+      char = char - 26
+    elif(ord(letter) < 97 or ord(letter) > 122):
+      new_msg += letter
+      continue
+    elif(char > 71 and char < 97):
+      char = char + 26
+    new_msg += chr(char)
+  print(new_msg)
+
+repeat = 'yes' ## default value
+#########  iterative loop
+while(repeat == 'yes'):
+  direction = input("Type 'encode' to encrypt, Type 'decode' to decrypt:\n")
+  msg = input("Type your message:\n").lower()
+  shift = int(input("Type shift number:\n"))
+  caeser(cipher_direction = direction, shift = shift, msg = msg)
+  repeat = input("Type 'yes' if you want to go again, otherwise type 'no'.\n")
+
     
 
