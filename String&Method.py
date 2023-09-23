d11cf6d2cd3a3903285d36530919f19b3b3568e3
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-09-23 22:55:41 +0530

    Understanding String & Its Method

---
+++ String&Method.py
@@ -0,0 +1,21 @@
+import math
+print("The square root of {} is : {}".format(64, math.sqrt(64)))
+var = '''
+Welcome to
+  Learning of
+    String in Python
+'''
+print(var[9])
+print(var[-2])
+s = "HELLO PYTHON"
+print(s[6:12])
+print(s[0:-7])
+li = list(s)
+li.remove('E')
+li.insert(1, 'O')
+print(li)
+sb = "WORLD"
+print(s*3)
+print("My name is %s and my age is : %d" % ("Shashi Anand", 25))
+f = 232.34455657
+print("%d" % (f))
