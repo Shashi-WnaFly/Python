97fa68f74075ee1323c7444282c314827f8a3de8
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-09-30 22:08:38 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+tup1 = (1,2,3,4,5,6,7,8)
+tup2 = ("ox", "cow", "buffalo", "camel")
+tup3 = ("shasy", 23, 121.32, 3+3j, False)
+
+# print(tup1[5])
+# print(tup3[1:4])
+
+# tup1[4] = 99 produce an error
+# print(tup1)
+
+# print(f"tup1 : {id(tup1)}") 
+# l1 = list(tup1)
+# l1.append(9)        tup1 object are different
+# tup1 = tuple(l1)
+# print(f"tup1 : {id(tup1)}")
+
+# a, b, c, d, e, f, g = tup1  this will produce an error because of one variable is missing.
+# a, b, c, d, e, f, g, h = tup1
+# print(c) // ot --> 3
